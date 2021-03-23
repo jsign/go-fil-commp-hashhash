9581092b06a0fa39d31cf20c9de4a2f86bcdf91e
Ignacio Hagopian <jsign.uy@gmail.com>
2021-03-22 21:09:04 -0300

    .
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- commp_test.go
+++ commp_test.go
@@ -1,6 +1,127 @@
 package commp
 
-import "testing"
+import (
+	"context"
+	"flag"
+	"fmt"
+	"io"
+	"math/rand"
+	"os"
+	"sync"
+	"testing"
+
+	commcid "github.com/filecoin-project/go-fil-commcid"
+	"github.com/ipfs/go-car"
+	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-cidutil"
+	chunker "github.com/ipfs/go-ipfs-chunker"
+	files "github.com/ipfs/go-ipfs-files"
+	"github.com/ipfs/go-merkledag"
+	dstest "github.com/ipfs/go-merkledag/test"
+	"github.com/ipfs/go-unixfs/importer/balanced"
+	ihelper "github.com/ipfs/go-unixfs/importer/helpers"
+	mh "github.com/multiformats/go-multihash"
+)
+
+var outcar = flag.Bool("outcar", false, "Output generated CAR files")
 
 func TestCommP(t *testing.T) {
+	cid, r, err := genCar(1, 1<<20)
+	if err != nil {
+		t.Fatal(err)
+	}
+	var wg sync.WaitGroup
+
+	pr, pw := io.Pipe()
+	if *outcar {
+		wg.Add(1)
+		r = io.TeeReader(r, pw)
+		go func() {
+			defer wg.Done()
+			f, err := os.Create(cid.String())
+			if err != nil {
+				panic(err)
+			}
+			defer func() {
+				if err := f.Close(); err != nil {
+					panic(err)
+				}
+			}()
+			if _, err = io.Copy(f, pr); err != nil {
+				panic(err)
+			}
+		}()
+	}
+
+	cp := NewAccumulator()
+	n, err := io.Copy(cp, r)
+	if err != nil {
+		t.Fatal(err)
+	}
+	pw.Close()
+
+	rawCommP, paddedSize, err := cp.Digest()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	commCid, err := commcid.DataCommitmentV1ToCID(rawCommP)
+
+	fmt.Printf(`Finished:
+CommP:    %x
+CommPCid: %s
+Raw bytes:      % 12d bytes
+Unpadded piece: % 12d bytes
+Padded piece:   % 12d bytes
+		`,
+		rawCommP,
+		commCid,
+		n,
+		paddedSize/128*127,
+		paddedSize,
+	)
+
+	wg.Wait()
+}
+
+func genCar(seed, size int64) (cid.Cid, io.Reader, error) {
+	rs := rand.New(rand.NewSource(seed))
+
+	rf := io.LimitReader(rs, size)
+	z := files.NewReaderFile(rf)
+
+	prefix, err := merkledag.PrefixForCidVersion(1)
+	if err != nil {
+		return cid.Undef, nil, err
+	}
+	prefix.MhType = uint64(mh.BLAKE2B_MIN + 31)
+
+	dserv := dstest.Mock()
+	params := ihelper.DagBuilderParams{
+		Maxlinks:  1024,
+		RawLeaves: true,
+		CidBuilder: cidutil.InlineBuilder{
+			Builder: prefix,
+			Limit:   126,
+		},
+		Dagserv: dserv,
+	}
+
+	db, err := params.New(chunker.NewSizeSplitter(z, 1<<20))
+	if err != nil {
+		return cid.Undef, nil, err
+	}
+	nd, err := balanced.Layout(db)
+	if err != nil {
+		return cid.Undef, nil, err
+	}
+	rr, rw := io.Pipe()
+	go func() {
+		defer rw.Close()
+		if err := car.WriteCar(context.Background(), dserv, []cid.Cid{nd.Cid()}, rw); err != nil {
+			panic(err) // TODO: fix this
+		}
+	}()
+
+	return nd.Cid(), rr, nil
 }
--- go.mod
+++ go.mod
@@ -3,8 +3,16 @@ module github.com/filecoin-project/go-fil-commp-hashhash
 go 1.11
 
 require (
-	github.com/filecoin-project/go-fil-commcid v0.0.0-20201016201715-d41df56b4f6a // indirect
+	github.com/filecoin-project/go-fil-commcid v0.0.0-20201016201715-d41df56b4f6a
+	github.com/ipfs/go-car v0.0.4
+	github.com/ipfs/go-cid v0.0.6
+	github.com/ipfs/go-cidutil v0.0.2
+	github.com/ipfs/go-ipfs-chunker v0.0.1
+	github.com/ipfs/go-ipfs-files v0.0.8
+	github.com/ipfs/go-merkledag v0.3.2
+	github.com/ipfs/go-unixfs v0.2.4
 	github.com/minio/sha256-simd v1.0.0
+	github.com/multiformats/go-multihash v0.0.14
 	github.com/pborman/options v1.2.0 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1
 )
